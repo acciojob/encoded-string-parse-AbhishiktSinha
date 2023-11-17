4d504b860c897eb998a15dfd81aa037787b9a906
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-18 00:56:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 const parseCode = (str) => {
   // your code here
+	let decodedObj = {
+        firstName: "",
+        lastName: "", 
+        id: ""
+    };
+
+    let objPropArray = ["firstName", "lastName", "id"];
+    let propIndex = 0;
+
+    let i = 0;
+    while(i < str.length)
+        {if(str.charAt(i) == '0') {
+            i++;
+        }
+        else{
+            let nextZero = str.indexOf('0', i);
+            let value;
+            if(nextZero == -1)
+                {value = str.slice(i);
+                    i = str.length;
+                }
+            else
+                {value = str.slice(i, nextZero);
+                    i = nextZero;
+                } 
+            
+            decodedObj[objPropArray[propIndex++]] = value;  
+		}}
+	return decodedObj;
 };
 
 // Do not change the code below
